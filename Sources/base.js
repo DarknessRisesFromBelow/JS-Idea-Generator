f0209026f71c2771576e35050564a5de39c8a4e3
YairMedina <99030228+DarknessRisesFromBelow@users.noreply.github.com>
2023-12-01 22:52:25 +0200

    Update base.js

--- Sources/base.js
+++ Sources/base.js
@@ -1,12 +1,13 @@
-let a = ["fata","hamelech", "met", "aleicha"] 
+let a = ["pizza","code", "snake", "apple", "app", "tea", "tree", "binary", "zip", "simulation", "free"] 
+let b_words = ["with", "without", "application", "runner", "executer", "parsed", "gamified", "generates"]
 
 function name(){
-	let value1 = "itay";
-	let value2 = Math.floor(Math.random() * 3);
-	let value3 = Math.floor(Math.random() * 3);
+	let value1 = Math.floor(Math.random() * a.length());
+	let value2 = Math.floor(Math.random() * b_words.length());
+	let value3 = Math.floor(Math.random() * a.length());
 
-	let b = value1;
-	let c = a[value2];
+	let b = a[value1];
+	let c = b_words[value2];
 	let d = a[value3];
 
 	let e = b + " " + c + " " + d 
